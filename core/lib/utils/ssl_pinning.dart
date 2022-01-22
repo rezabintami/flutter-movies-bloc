22fb56a76b8062404cefd3dbf95cebb4aafe804a
Muhammad Reza Bintami <rezabintami@gmail.com>
2022-01-23 01:22:44 +0700

    add library crashlytics and ssl pinning

--- core/lib/utils/ssl_pinning.dart
+++ core/lib/utils/ssl_pinning.dart
@@ -19,7 +19,7 @@ class Shared {
     SecurityContext context = SecurityContext(withTrustedRoots: false);
     try {
       List<int> bytes = [];
-      bytes = (await rootBundle.load('certificates/certificates.cer'))
+      bytes = (await rootBundle.load('certificated/ssl-pinning.pem'))
           .buffer
           .asUint8List();
       context.setTrustedCertificatesBytes(bytes);
